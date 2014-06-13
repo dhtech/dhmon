32af837159803355c3c26390aab4c275c4f0a1c1
Christian Svensson <blue@cmd.nu>
2014-06-14 00:58:12 +0200

    Removed RTG support scripts, updated dhmon for bulk updates

--- src/metrics/dhmon.py
+++ src/metrics/dhmon.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+import collections
 import socket
 import sys
 import time
+
+
+BulkMetric = collections.namedtuple('BulkMetric', [
+  'timestamp', 'hostname', 'metric', 'value'])
+
 
 def metric(metric, value, hostname=None, timestamp=None):
     if timestamp is None:
@@ -24,5 +30,27 @@ def metric(metric, value, hostname=None, timestamp=None):
 
     graphite_msg = '%s %s %s\n' % (path, value, timestamp)
     graphite_socket.send( graphite_msg )
+    graphite_socket.close()
+    return True
+
+
+def metricbulk(values):
+    graphite_address = ( 'metricstore.event.dreamhack.se', 2003 )
+    try:
+      graphite_socket = socket.socket()
+      graphite_socket.connect( graphite_address )
+    except Exception as e:
+      print ( 'Could not connect to Graphite on %s, port %d, %s',
+          graphite_address[0], graphite_address[1], e )
+      return False
+
+    for bulkmetric in values:
+      rev_hostname = '.'.join( reversed( bulkmetric.hostname.split('.') ) )
+      path = 'dh.%s.%s' % ( rev_hostname, bulkmetric.metric )
+
+      graphite_msg = '%s %s %s\n' % (path, bulkmetric.timestamp,
+          bulkmetric.timestamp)
+      graphite_socket.send( graphite_msg )
+
     graphite_socket.close()
     return True
