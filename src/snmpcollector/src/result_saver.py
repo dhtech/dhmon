1881059ace955147332d779608534c629ea41602
Christian Svensson <blue@cmd.nu>
2015-06-11 13:57:06 +0200

    Naive Prometheus instrumentation

--- src/snmpcollector/src/result_saver.py
+++ src/snmpcollector/src/result_saver.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2
 import base64
 import logging
+import prometheus_client
 import time
 
 import config
 import stage
 
+
+HTTP_PORT = 13100
 
 OID_ifDescr = '.1.3.6.1.2.1.2.2.1.2.'
 
@@ -22,12 +25,15 @@ class SaveAction(stage.Action):
 
 class ResultSaver(stage.Stage):
 
-  INTEGER_TYPES = ['COUNTER', 'COUNTER64', 'INTEGER', 'TICKS', 'GAUGE']
+  NUMERIC_TYPES = ['COUNTER', 'COUNTER64', 'INTEGER', 'TICKS', 'GAUGE']
 
   def __init__(self):
     super(ResultSaver, self).__init__('result_saver', task_queue='worker')
+    self.save_time = prometheus_client.Summary(
+      'result_saver_processing_seconds', 'Time spent in do_save')
     self.mibresolver = None
     self.mibcache = {}
+    self.metrics = {}
 
   def startup(self):
     import mibresolver
@@ -35,6 +41,10 @@ class ResultSaver(stage.Stage):
     super(ResultSaver, self).startup()
 
   def do_save(self, target, results):
+    with self.save_time.time():
+      self._save(target, results)
+
+  def _save(self, target, results):
     if_oids = config.get('saver', 'if-oids')
     timestamp = int(target.timestamp)
 
@@ -74,17 +84,36 @@ class ResultSaver(stage.Stage):
           break
 
       value = result.value
-      if result.type in self.INTEGER_TYPES:
-        value = int(value)
+      if result.type in self.NUMERIC_TYPES:
+        self.export_numeric(target, result, mib, obj, index, interface)
       else:
-        value = '%s:%s' % (result.type, base64.b64encode(value))
-
+        self.export_blob(target, result, mib, obj, index, interface)
       saved += 1
 
     # Save collection stats
     logging.debug('Save completed for %d metrics for %s', saved, target.host)
 
+  def export_numeric(self, target, result, mib, obj, index, interface):
+    metric = self.metrics.get(obj, None)
+    if not metric:
+      if result.type == 'COUNTER64' or result.type == 'COUNTER':
+        metric = prometheus_client.Counter(obj, '%s::%s' % (mib, obj),
+            ['device', 'index', 'interface', 'type'])
+      else:
+        metric = prometheus_client.Gauge(obj, '%s::%s' % (mib, obj),
+            ['device', 'index', 'interface', 'type'])
+      self.metrics[obj] = metric
+
+    instance = metric.labels(target.host, index, interface, result.type)
+    # HACK: metrics only support delta increments currently
+    with instance._lock:
+      instance._value = int(result.value)
+
+  def export_blob(self, target, result, mib, obj, index, interface):
+    # TODO
+    pass
 
 if __name__ == '__main__':
+  prometheus_client.start_http_server(HTTP_PORT)
   stage = ResultSaver()
   stage.run()
