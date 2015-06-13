742ea2a5f0d2e28bf9127a34ca8b9af765ee07b6
Christian Svensson <blue@cmd.nu>
2015-06-14 00:22:29 +0200

    Add timer to snmptest and remove some bitrot

--- src/snmpcollector/src/snmptest.py
+++ src/snmpcollector/src/snmptest.py
@@ -5,18 +5,26 @@ import sys
 import time
 
 import supervisor
-import snmp_worker
+import worker
 
+
+host = sys.argv[1]
+oid = None
+if len(sys.argv) > 2:
+  oid = sys.argv[2]
+  del sys.argv[2]
+del sys.argv[1]
 
 logging.basicConfig(level=logging.DEBUG)
 
 supervisor_stage = supervisor.Supervisor()
-worker_stage = snmp_worker.SnmpWorker()
+worker_stage = worker.Worker()
 
 targets = {host: target for host, target
            in supervisor_stage.construct_targets(time.time())}
 
-target = targets.get(sys.argv[1], None)
+start = time.time()
+target = targets.get(host, None)
 if not target:
   print 'Target not found'
   exit(1)
@@ -30,10 +38,15 @@ print 'Target model:', model
 
 print 'Target VLANs:', target.vlans()
 
-if len(sys.argv) > 2:
-  for i in target.walk(sys.argv[2]).iteritems():
+if oid:
+  for i in target.walk(oid).iteritems():
     print i
 else:
   for action in worker_stage.do_snmp_walk(target):
     for key in sorted(action.results.keys()):
       print key, action.results[key]
+for action in worker_stage.do_snmp_walk(target):
+  for key in sorted(action.results.keys()):
+    print key, action.results[key]
+
+print 'Duration:', time.time() - start
