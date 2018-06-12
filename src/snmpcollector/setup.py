3a85f4c85d2d35e8f17fef358161886ee2a10f58
Christian Svensson <blue@cmd.nu>
2018-06-12 13:43:52 +0200

    remove old snmpcollector, use snmpexporter instead

--- src/snmpcollector/setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name='mibresolver', version='0.1', ext_modules=[
-  Extension('mibresolver', sources=['src/mibresolver.c'], libraries=['netsnmp'])])
